cb645b06586c257c9e954559f1b88430bdf7e0d7
saniho <52693545+saniho@users.noreply.github.com>
2021-03-10 22:02:14 +0100

    addListePort dans le test

--- custom_components/apiMareeInfo/const.py
+++ custom_components/apiMareeInfo/const.py
@@ -8,6 +8,6 @@ DOMAIN = "apiMareeInfo"
 # delai pour l'update http, toutes les 3 heures
 SCAN_INTERVAL_http = datetime.timedelta(seconds=60*60*3)
 
-__VERSION__ = "v1.0.1.4RC"
+__VERSION__ = "v1.0.1.4"
 
 __name__ = "apiMareeInfo"
