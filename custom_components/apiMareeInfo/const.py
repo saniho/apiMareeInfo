c024a85f8f3b5e1514060fdc78b31f554bfa77a7
saniho <sanihogithub@gmail.com>
2026-01-26 22:40:13 +0100

    version 1.4.0 Ajout temperature de l'eau

--- custom_components/apiMareeInfo/const.py
+++ custom_components/apiMareeInfo/const.py
@@ -5,6 +5,7 @@ import datetime
 ISSUE_URL = "https://github.com/saniho/apiMareeInfo/issues"
 
 DOMAIN = "apiMareeInfo"
+PLATFORMS = ["sensor"]
 
 # delai pour l'update http, toutes les 3 heures
 CONF_SCAN_INTERVAL_HTTP = datetime.timedelta(seconds=60 * 60 * 3)
