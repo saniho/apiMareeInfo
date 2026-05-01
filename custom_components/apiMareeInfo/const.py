69f5d3d0b2331f42e7ca47a50eaa6dab63152764
saniho <sanihogithub@gmail.com>
2026-05-01 22:11:52 +0200

    chore: release version 2.0.0

--- custom_components/apiMareeInfo/const.py
+++ custom_components/apiMareeInfo/const.py
@@ -18,6 +18,6 @@ PROVIDER_STORMGLASS = "Stormglass.io"
 DEFAULT_PROVIDER = PROVIDER_MAREEINFO
 PROVIDERS = [PROVIDER_MAREEINFO]
 
-__VERSION__ = "1.6.0-beta.15"
+__VERSION__ = "2.0.0"
 
 __name__ = "apiMareeInfo"
