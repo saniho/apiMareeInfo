55a7a6a1c146ae5ba794fe80811c9f0d80cc27e9
saniho <saniho@users.noreply.github.com>
2026-09-14 15:53:48 +0000

    fix: revert version to 2.1.6 - HA rejects dots in beta version key

--- custom_components/apiMareeInfo/const.py
+++ custom_components/apiMareeInfo/const.py
@@ -20,6 +20,6 @@ PROVIDER_STORMGLASS = "Stormglass.io"
 DEFAULT_PROVIDER = PROVIDER_MAREEINFO
 PROVIDERS = [PROVIDER_MAREEINFO]
 
-__VERSION__ = "2.1.7.beta.1"
+__VERSION__ = "2.1.6"
 
 __name__ = "apiMareeInfo"
